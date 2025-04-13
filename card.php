72305a4840ab00ac5c47e6a5ab606aa6f15ca5dc
MDW <mdeweerd@users.noreply.github.com>
2025-04-13 17:44:47 +0200

    Try to show menu

--- card.php
+++ card.php
@@ -16,10 +16,31 @@ $client_id = GETPOST('id', 'int');
 require_once DOL_DOCUMENT_ROOT.'/custom/extraitcompte/class/ExtraitCompte.class.php';
 $extraitCompte = new ExtraitCompte($db);
 
-// Display the extract
+// Display the header with the tab list
 llxHeader('', 'Extrait de Compte');
+print dol_get_fiche_head(prepareHead($extraitCompte, $client_id), 'extraitcompte', $langs->trans("ExtraitCompte"), 0, 'bill');
+
+// Display the extract
 $extraitCompte->displayExtraitCompte($client_id);
+
+// Display the footer with the tab list
+print dol_get_fiche_end();
 llxFooter();
 
 $db->close();
 
+function prepareHead($extraitCompte, $client_id) {
+    global $langs;
+
+    $head = array();
+    $head[] = array(
+        'text' => $langs->trans("ExtraitCompte"),
+        'url' => dol_buildpath('/custom/extraitcompte/card.php', 1) . '?id=' . $client_id,
+        'active' => 1
+    );
+
+    // Add other tabs if needed
+    // $head[] = array(...);
+
+    return $head;
+}
